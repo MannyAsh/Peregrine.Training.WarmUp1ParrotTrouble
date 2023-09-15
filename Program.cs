b7d867ee8cef04bf0949c8443bfd57f3cc7690f1
MannyAsh <amaan.ashiq.aa@gmail.com>
2023-09-15 15:54:55 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarmUp1ParrotTrouble
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            Console.WriteLine(parrotTrouble(true, 6));
+            Console.WriteLine(parrotTrouble(true, 7));
+            Console.WriteLine(parrotTrouble(false, 6));
+            Console.ReadLine();
+
+
+        }
+
+        static Boolean parrotTrouble(Boolean talking, int hour) {
+
+            if (talking == true && hour < 7 || hour > 20)
+            {
+
+                return true;
+
+            }
+
+            else {
+
+                return false;
+            
+            }
+        
+        }
+    }
+}
